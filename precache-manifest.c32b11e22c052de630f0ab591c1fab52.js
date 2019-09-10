8b17e3ec87b63afa99b12b69ac140b51697ed2c7
TuGuang <tuguang2013@gmail.com>
2019-09-10 13:19:12 +0800

    init

---
+++ precache-manifest.c32b11e22c052de630f0ab591c1fab52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f352593ee65831af678a5a478ccd24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c03c258dbb891713717d",
+    "url": "/static/css/main.bcac3eae.chunk.css"
+  },
+  {
+    "revision": "66437571249c572437bb",
+    "url": "/static/js/2.25ee865f.chunk.js"
+  },
+  {
+    "revision": "c03c258dbb891713717d",
+    "url": "/static/js/main.f7362561.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
