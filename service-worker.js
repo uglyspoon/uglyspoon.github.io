21fe6c6e8cf6f670133a893c9fd21260c899ad9e
TuGuang <tuguang2013@gmail.com>
2019-09-16 09:32:01 +0800

    marjor functions done

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9deaff1c1d40f4886f0e0e0ab8bcebd.js"
+  "/precache-manifest.4a1a0744aa1aa8ecbca11b36108b437a.js"
 );
 
 self.addEventListener('message', (event) => {
