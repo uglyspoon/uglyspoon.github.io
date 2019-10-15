96771f1b3ddf714e1789987b8e72980e1b5c9097
TuGuang <tuguang2013@gmail.com>
2019-10-15 16:19:10 +0800

    remote done

---
+++ labelpeopleserver/precache-manifest.393b9009049693bfb839add7aa0fee42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47287380478120dd851848ff8ce4dad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c114a389ab4c76d6562",
+    "url": "./static/css/2.1782e1af.chunk.css"
+  },
+  {
+    "revision": "cf903b3e6456e2df4065",
+    "url": "./static/css/main.0ce6ce1a.chunk.css"
+  },
+  {
+    "revision": "8c114a389ab4c76d6562",
+    "url": "./static/js/2.92a00161.chunk.js"
+  },
+  {
+    "revision": "cf903b3e6456e2df4065",
+    "url": "./static/js/main.9e8ac53e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
