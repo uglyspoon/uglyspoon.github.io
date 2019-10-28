f2db28121c74c8eee9ef7c753dd339e40b6a38c5
TuGuang <tuguang2013@gmail.com>
2019-10-28 15:32:07 +0800

    add pcscroe

--- pcscore/service-worker.js
+++ pcscore/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c6c1170e4dad890be25044dce2ad3cdc.js"
+  "/precache-manifest.a81146aa3c05cf675427a42c655a1215.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
