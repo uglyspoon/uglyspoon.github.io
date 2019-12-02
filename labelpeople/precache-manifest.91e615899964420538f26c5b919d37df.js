4d5cfe079ed00d93e5570fcb8fe46aa42935349a
TuGuang <tuguang2013@gmail.com>
2019-12-02 14:55:29 +0800

    regular update

---
+++ labelpeople/precache-manifest.91e615899964420538f26c5b919d37df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b66e477fd3cdb0fac6a8d2d96830741",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e768a83b56b969a19f8",
+    "url": "./static/css/2.1782e1af.chunk.css"
+  },
+  {
+    "revision": "41c010ef044553053fd2",
+    "url": "./static/css/main.c7ecb37c.chunk.css"
+  },
+  {
+    "revision": "8e768a83b56b969a19f8",
+    "url": "./static/js/2.e8b037cc.chunk.js"
+  },
+  {
+    "revision": "41c010ef044553053fd2",
+    "url": "./static/js/main.5a62228d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "867ab58420658a8d4a3b6c41a2c764d3",
+    "url": "./static/media/dir.867ab584.svg"
+  }
+]);
--- labelpeople/service-worker.js
+++ labelpeople/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/labelpeople/precache-manifest.a9a9ff9901249fa202960630bc708d19.js"
+  "./precache-manifest.91e615899964420538f26c5b919d37df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/labelpeople/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
